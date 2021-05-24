fb1b24ef5a1a5caa891bef35cf30130e668c05f8
Ulysses Lin <utemoc@gmail.com>
2021-05-24 10:29:52 -0700

    Routes technician-list to technician-details

--- SalonSpaceAngular/src/app/app-routing.module.ts
+++ SalonSpaceAngular/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TechnicianListComponent } from './technician-list/technician-list.component';
+import { TechnicianDetailsComponent } from './technician-details/technician-details.component';
 import { WelcomeComponent} from './welcome/welcome.component';
 import { RatingPublishedComponent} from './rating-published/rating-published.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'technician', component: TechnicianListComponent },
+  { path: 'technician-details', component: TechnicianDetailsComponent }
 ];
 
 @NgModule({
