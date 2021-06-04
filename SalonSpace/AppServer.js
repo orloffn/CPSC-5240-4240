b25001381df4db5ae0b6892b522a30e76bbaf235
Ulysses Lin <utemoc@gmail.com>
2021-06-03 22:18:54 -0700

    Trivial change to try deployment

--- SalonSpace/AppServer.js
+++ SalonSpace/AppServer.js
@@ -3,4 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const App_1 = require("./App");
 let server = new App_1.App().expressApp;
 server.listen(process.env.PORT || 8080);
-console.log("Server running on port:8080 or Azure port");
+console.log("Server running on port:8080 or Azure port.");
--- SalonSpace/AppServer.ts
+++ SalonSpace/AppServer.ts
@@ -8,4 +8,4 @@ import {App} from './App';
 
 let server: any = new App().expressApp;
 server.listen(process.env.PORT||8080);
-console.log("Server running on port:8080 or Azure port");
+console.log("Server running on port:8080 or Azure port.");
