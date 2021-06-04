b8fdbe7f5b6405560af82b3ec157c7a3bbd418ca
Ulysses Lin <utemoc@gmail.com>
2021-06-03 20:00:08 -0700

    UL deploy 1

--- SalonSpace/DataAccess.js
+++ SalonSpace/DataAccess.js
@@ -1,26 +1,25 @@
 "use strict";
-exports.__esModule = true;
+Object.defineProperty(exports, "__esModule", { value: true });
 exports.DataAccess = void 0;
-var Mongoose = require("mongoose");
-var DataAccess = /** @class */ (function () {
-    function DataAccess() {
+const Mongoose = require("mongoose");
+class DataAccess {
+    constructor() {
         // DataAccess.connect();
         // { useUnifiedTopology: true };
         //{ useNewUrlParser: true };
     }
-    DataAccess.connect = function () {
+    static connect() {
         if (this.mongooseInstance)
             return this.mongooseInstance;
         this.mongooseConnection = Mongoose.connection;
-        this.mongooseConnection.on("open", function () {
+        this.mongooseConnection.on("open", () => {
             console.log("Connected to mongodb.");
         });
         this.mongooseInstance = Mongoose.connect(this.DB_CONNECTION_STRING);
         return this.mongooseInstance;
-    };
-    //static DB_CONNECTION_STRING:string = 'mongodb://dbAdmin:test@localhost:3000/SalonSpace?authSource=admin';
-    DataAccess.DB_CONNECTION_STRING = 'mongodb+srv://SalonSpace123:SalonSpace123@cluster0.txxl6.mongodb.net/SalonSpace?retryWrites=true&w=majority';
-    return DataAccess;
-}());
+    }
+}
 exports.DataAccess = DataAccess;
+//static DB_CONNECTION_STRING:string = 'mongodb://dbAdmin:test@localhost:3000/SalonSpace?authSource=admin';
+DataAccess.DB_CONNECTION_STRING = 'mongodb+srv://SalonSpace123:SalonSpace123@cluster0.txxl6.mongodb.net/SalonSpace?retryWrites=true&w=majority';
 DataAccess.connect();
